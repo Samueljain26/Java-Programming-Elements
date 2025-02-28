2c5fa210cf15cb999d8770f89da6f3e1e7782930
Samueljain26 <samueljain77@gmail.com>
2025-02-28 10:04:16 +0530

    [Samuel] UPDATE: Converting Kilometers to Miles

---
+++ ConvertDistanceToMiles.java
@@ -0,0 +1,8 @@
+class ConvertDistanceToMiles{
+        public static void main (String[] args){
+        double distanceInKilometers =10.8;
+        double distanceInMiles = distanceInKilometers*1.6;
+        System.out.print("The Distance " + distanceInKilometers + "km in miles is: " + distanceInMiles);
+}
+}
+
