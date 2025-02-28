b1e3112e1c323e437fb1b0ee08036a5cdc0f7df3
Samueljain26 <samueljain77@gmail.com>
2025-02-28 09:46:42 +0530

    [Samuel] ADD: Age of henry

---
+++ AgeOfHarry.java
@@ -0,0 +1,9 @@
+class AgeOfHarry{
+	public static void main (String[] args){
+	int currentYear =2024;
+	int birthYear =2000;
+	int age = currentYear - birthYear;
+	System.out.print("Harry's age in" + currentYear + " is: " + age);
+}
+}
+
