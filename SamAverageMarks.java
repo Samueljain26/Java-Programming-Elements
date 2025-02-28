d9eab81abe120df0b94bd2992aef0eed13a38f80
Samueljain26 <samueljain77@gmail.com>
2025-02-28 09:54:00 +0530

    [Samuel] ADD: Sam's average marks in PCM

---
+++ SamAverageMarks.java
@@ -0,0 +1,10 @@
+class SamAverageMarks{
+        public static void main (String[] args){
+        int marksInMaths =94;
+        int marksInPhysics =95;
+	int marksInChemistry =96;
+	int averageMarks = (marksInMaths + marksInPhysics + marksInChemistry)/3;
+
+        System.out.print("Sam’s average mark in PCM is: " + averageMarks);
+}
+}
